70764ec6536eeb9657a16b4f69f37b2b0ac28335
munna3568 <bdbdnnxnd8@gmail.com>
2026-09-18 13:04:22 +0600

    Add new map mode for Free Fire

--- bot.py
+++ bot.py
@@ -28,6 +28,7 @@ FREE_MAPS = {
 "map3": ["3. যমুনা সেতু ম্যাপ", "https://drive.google.com/drive/folders/1A_0zEQ9e8QIDLPjRbT7PmjXDZFap4V8Z"], 
 "map4": ["4. রাজশাহী টু বরিশাল", "https://drive.google.com/drive/folders/1FheeZ49p62z4YWxWKSU22R_2RjC4iALg"], 
 "map5": ["5. ঢাকা সিটি ম্যাপ মোড", "https://youtu.be/tttZqNFDO4c?si=11WOdhQGkTcEkK6f"] 
+    "map6": ["6. ফ্রি ফায়ার ম্যাপ মোড", "https://drive.google.com/drive/folders/1kWNnevLzQlKuTcbEOMhVOn-sgz9VnH-t"]
 }
 PAID_MAPS = {
     "p5": ["5. ঢাকা টু চট্টগ্রাম", "https://drive.google.com/drive/folders/169GKO51_Dyg6XEAkLO0hrT2WymW87cbM"],
